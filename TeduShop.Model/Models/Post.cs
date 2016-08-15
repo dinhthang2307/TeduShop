eec1171472b55d2f2546aef13ecb44f5115e8e91
dinhthang2307 <dinhthang2307@gmail.com>
2016-08-15 20:57:02 +0700

    Bài_05

---
+++ TeduShop.Model/Models/Post.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Posts")]
+    public class Post : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { set; get; }
+
+        [Required]
+        [Column(TypeName = "varchar")]
+        [MaxLength(256)]
+        public string Alias { set; get; }
+
+        [Required]
+        public int CategoryID { set; get; }
+
+     
+        [MaxLength(256)]
+        public string Image { set; get; }
+
+        public decimal? Price { set; get; }
+
+       
+        [MaxLength(500)]
+        public string Description { set; get; }
+
+        public bool? HomeFlag { set; get; }
+        public bool? HotFlag { set; get; }
+        public int? viewCount { set; get; }
+
+        public string Content { set; get; }
+
+        [ForeignKey("CategoryID")]
+        public virtual PostCategory PostCategory { set; get; }
+    }
+}
