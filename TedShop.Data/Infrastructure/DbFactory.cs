bbe3dfbd9538097e1532dd7609057df2804527bf
dinhthang2307 <dinhthang2307@gmail.com>
2016-08-18 08:42:24 +0700

    add etityfr for datamodel

---
+++ TedShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,23 @@
+﻿using TeduShop.Data;
+using TeduShop.Data.Infrastructure;
+
+namespace TedShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeduShopDbContext dbContext;
+
+        public TeduShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+            {
+                dbContext.Dispose();
+            }
+        }
+    }
+}
---
+++ TedShop.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TedShop.Data.Infrastructure;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TedShop.Data.Repositories
+{
+    public class ProductCategoryRepository : IRepository<ProductCategory>
+    {
+        public ProductCategoryRepository(DbFactory:base(DbFactory dbFactory){
+
+            }
+    }
+}
