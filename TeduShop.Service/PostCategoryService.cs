f459bf9dd4f9277a6fc3712b447bbf465122a1f3
dinhthang2307 <dinhthang2307@gmail.com>
2016-09-08 16:27:22 +0700

    add WebApi Resful

--- TeduShop.Service/PostCategoryService.cs
+++ TeduShop.Service/PostCategoryService.cs
@@ -16,6 +16,7 @@ namespace TeduShop.Service
         IEnumerable<PostCategory> GetAll();
         IEnumerable<PostCategory> GetAllByParentID(int parentId);
         PostCategory GetById(int id);
+        void Save();
 
     }
     public class PostCategoryService : IPostCategoryService
@@ -51,6 +52,11 @@ namespace TeduShop.Service
         public PostCategory GetById(int id)
         {
             return _postCateoryRepository.GetSingleById(id);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
         }
 
         public void Update(PostCategory postCategory)
